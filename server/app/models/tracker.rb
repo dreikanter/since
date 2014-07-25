28770ea914ea7fed765fafa680f151023cbcecac
Alex Musayev <alex.musayev@gmail.com>
2014-07-25 16:15:10 +0400

    Tracker model

--- server/app/models/tracker.rb
+++ server/app/models/tracker.rb
@@ -1,7 +1,7 @@
 class Tracker < ActiveRecord::Base
   validates :name, presence: true
   validates_format_of :secret, with: /\A[a-z\d_-]{1,100}$\z/i
-  validates :period, inclusion: { in: 1..365*24 }
+  # Validate :cron is a valid cron string
   belongs_to :user
   has_many :notifications, dependent: :destroy
 end
