f8b04837c7a04044e85badde3e2cad6a32359c13
Alex Musayev <alex.musayev@gmail.com>
2014-07-26 00:55:35 +0400

    Users controller updated using responders

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 require 'pp'
 class UsersController < ApplicationController
+  respond_to :json
+
   def index
-    @users = User.all
-    render json: @users
+    respond_with User.all
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
+    respond_with User.where(id: params[:id]).first
   end
 end
--- server/spec/controllers/users_controller_spec.rb
+++ server/spec/controllers/users_controller_spec.rb
@@ -4,13 +4,13 @@ require 'spec_helper'
 RSpec.describe UsersController, :type => :controller do
   describe "GET #index" do
     it "returns 200 status code" do
-      get :index
+      get :index, format: :json
       expect(response.status).to eq(200)
     end
   end
 
   it "returns 200 status code" do
-    get :show, id: 1
+    get :show, id: 1, format: :json
     expect(response.status).to eq(200)
   end
 end
