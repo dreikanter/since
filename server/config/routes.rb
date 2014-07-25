13c3d26b09dbaf1ead3819f194bbb176747e41ba
Alex Musayev <alex.musayev@gmail.com>
2014-07-26 00:54:44 +0400

    API routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,11 @@
 Since::Application.routes.draw do
-  resources :users, only: [:index, :show]
+  get    '/users',              to: 'users#index'
+  get    '/users/:id',          to: 'users#show'
+  get    '/users/:id/trackers', to: 'trackers#belongings'
+  post   '/trackers/new',       to: 'trackers#create'
+  get    '/trackers/:id',       to: 'trackers#show'
+  put    '/trackers/:id',       to: 'trackers#update'
+  delete '/trackers/:id',       to: 'trackers#destroy'
+  get    '/push/:secret',       to: 'notifications#push'
+  get    '/push/:secret/test',  to: 'notifications#show'
 end
